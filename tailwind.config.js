77a6630a1ede61134a49a48136252c206be2c709
Tife <boluakindele12@gmail.com>
2023-02-22 19:22:47 +0100

    built the navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FAF6F2',
+        secondary: '#34251F',
+        inputbg: '#E3E3E3',
+      },
+      fontFamily: {
+        roboto: "'Roboto', sans-serif",
+        kepler: "'Kepler Std', sans-serif",
+      },
+    },
   },
   plugins: [],
 }
