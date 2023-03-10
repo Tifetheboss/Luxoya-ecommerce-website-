0468a02f779e679a6e46b60f4ee723de76bd748e
Tife <boluakindele12@gmail.com>
2023-03-10 12:02:41 +0100

    nav responsive

--- script.js
+++ script.js
@@ -1,28 +1,8 @@
-// tife's script //
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
 
-// const  days = document.getElementById('days');
-const hours = document.getElementById('hours');
-const  mins = document.getElementById('mins');
-const  secs = document.getElementById('secs');
-
-// const startTime = new Date().getTime();
-
-const endTime = new Date('January 07, 2024 00:00:00').getTime();
-
-//countdown
-function updateCountdown(){
-    const currentTime = new Date();
-    const diff = endTime - currentTime;
-
-    // const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
-    const m = Math.floor(diff / 1000 / 60 ) % 60;
-    const s = Math.floor(diff / 1000) % 60;
-
-    // days.innerHTML = d; 
-    hours.innerHTML = h < 10 ? '0' + h : h;
-    mins.innerHTML = m < 10 ? '0' + m : m;
-    secs.innerHTML = s < 10 ? '0' + s : s;
-}
-
-setInterval(updateCountdown, 1000);
+btn.addEventListener('click', () => {
+    btn.classList.toggle('open')
+    nav.classList.toggle('visible')
+    nav.classList.toggle('invisible')
+})
---
+++ script2.js
@@ -0,0 +1,39 @@
+// tife's script //
+
+// const  days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const  mins = document.getElementById('mins');
+const  secs = document.getElementById('secs');
+
+// const startTime = new Date().getTime();
+
+const endTime = new Date('January 07, 2024 00:00:00').getTime();
+
+//countdown
+function updateCountdown(){
+    const currentTime = new Date();
+    const diff = endTime - currentTime;
+
+    // const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60 ) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+    // days.innerHTML = d; 
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    mins.innerHTML = m < 10 ? '0' + m : m;
+    secs.innerHTML = s < 10 ? '0' + s : s;
+}
+
+setInterval(updateCountdown, 1000);
+
+
+// hamb //
+const hamburger = document.querySelector('.hamb');
+const nav = document.querySelector('.nav2');
+
+hamburger.addEventListener('click', function() {
+    this.classList.toggle('click');
+    nav.classList.toggle('open');
+  });
+// hamb ends//
